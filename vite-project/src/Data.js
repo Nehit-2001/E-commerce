c74fe0e3adfc1f2d564f58854a0ef57011ae873e
Mohit <mohitsn444@gmail.com>
2025-10-29 13:57:22 +0530

    First Commit

---
+++ vite-project/src/Data.js
@@ -0,0 +1,15 @@
+export const API_KEY = 'AIzaSyBbVVypyTKRJGIvm7XkSRGtiqS_AXusEIs';
+
+export const converter = (value)=>{
+    if(value>=1000000){
+        return Math.floor(value/1000000) + "M";
+    }
+
+    else if(value>=1000){
+        return Math.floor(value/1000) + "K";
+    }
+
+    else{
+        return value;
+    }
+}
---
+++ vite-project/src/Pages/Video/Video.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Video.css'
+
+import PlayVideo from '../../Component/PlayVideo/playVideo'
+import Recommended from '../../Component/Recommended/Recommended'
+import { useParams } from 'react-router-dom'
+
+const Video = () => {
+
+  const {videoId, categoryId} = useParams();
+
+  return (
+    <div className='play-container'>
+      <PlayVideo videoId={videoId}/>
+      <Recommended categoryId={categoryId}/>
+    </div>
+  )
+}
+
+export default Video
